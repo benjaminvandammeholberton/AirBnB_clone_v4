54a9dd2720ff85a601065467108d784935bf7369
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-09-27 13:15:27 +0200

    Improve code

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -35,59 +35,16 @@ $(document).ready(function () {
       $('div#api_status').removeClass('available');
     },
   });
-
   let jsonData = {};
-  $.ajax({
-    url: 'http://127.0.0.1:5001/api/v1/places_search/',
-    method: 'POST',
-    dataType: 'json',
-    data: JSON.stringify(jsonData),
-    contentType: 'application/json',
-    success: function (data) {
-      // Sort the data by place name
-      data.sort(function (a, b) {
-        return a.name.localeCompare(b.name);
-      });
-      $.each(data, function (index, place) {
-        var article = $('<article>');
-        var titleBox = $('<div class="title_box">');
-        var title = $('<h2>').text(place.name);
-        var priceByNight = $('<div class="price_by_night">').text(
-          '$' + place.price_by_night
-        );
-        var information = $('<div class="information">');
-        var maxGuest = $('<div class="max_guest">').text(
-          place.max_guest + (place.max_guest != 1 ? ' Guests' : ' Guest')
-        );
-        var numberRooms = $('<div class="number_rooms">').text(
-          place.number_rooms
-        );
-        var numberBathrooms = $('<div class="number_bathrooms">').text(
-          place.number_bathrooms
-        );
-        var user = $('<div class="user">').html(
-          '<b>Owner:</b> ' + place.user_id
-        );
-        var description = $('<div class="description">').html(
-          place.description
-        );
-
-        titleBox.append(title, priceByNight);
-        information.append(maxGuest, numberRooms, numberBathrooms);
-        article.append(titleBox, information, description);
-
-        $('.places').append(article);
-      });
-    },
-    error: function (xhr, status, error) {
-      // Handle errors here
-      console.error('Error fetching data:', error);
-    },
-  });
+  fetchDataAndRenderPlaces(jsonData);
 
   $('button').click(function () {
     $('.places').empty();
     let jsonData = { amenities: Object.keys(listChecked) };
+    fetchDataAndRenderPlaces(jsonData);
+  });
+
+  function fetchDataAndRenderPlaces(jsonData) {
     $.ajax({
       url: 'http://127.0.0.1:5001/api/v1/places_search/',
       method: 'POST',
@@ -135,5 +92,5 @@ $(document).ready(function () {
         console.error('Error fetching data:', error);
       },
     });
-  });
+  }
 });
